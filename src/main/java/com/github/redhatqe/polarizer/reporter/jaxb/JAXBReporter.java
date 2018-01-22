46dec85ed2eb62f1df0e1cab140ef3d454083613
Sean Toner <stoner@redhat.com>
2018-01-22 06:19:34 -0500

    Fixed bug in the xsd for testcases that would not make the Parameter available during jaxb generation

--- src/main/java/com/github/redhatqe/polarizer/reporter/jaxb/JAXBReporter.java
+++ src/main/java/com/github/redhatqe/polarizer/reporter/jaxb/JAXBReporter.java
@@ -2,6 +2,7 @@ package com.github.redhatqe.polarizer.reporter.jaxb;
 
 
 
+import com.github.redhatqe.polarizer.reporter.importer.testcase.Testcases;
 import com.github.redhatqe.polarizer.reporter.importer.xunit.Testcase;
 import com.github.redhatqe.polarizer.reporter.importer.xunit.Testsuite;
 import com.github.redhatqe.polarizer.reporter.importer.xunit.Testsuites;
@@ -18,7 +19,10 @@ public class JAXBReporter implements IJAXBHelper {
         if (t == Testcase.class || t == Testsuite.class || t == Testsuites.class) {
             xsd = JAXBReporter.class.getClassLoader().getResource("xunit_importers/xunit.xsd");
         }
-        else if (t == Testcase.class) {
+        else if (t == com.github.redhatqe.polarizer.reporter.importer.testcase.Testcase.class) {
+            xsd = JAXBReporter.class.getClassLoader().getResource("testcase_importer/testcase-importer.xsd");
+        }
+        else if (t == Testcases.class) {
             xsd = JAXBReporter.class.getClassLoader().getResource("testcase_importer/testcase-importer.xsd");
         }
         else
