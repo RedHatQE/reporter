9308546cb46bcc84700f8033a51c0c961234a527
Jan Stavěl @ RedHat <jstavel@redhat.com>
2022-05-24 21:35:06 +0200

    updated for java11 - jaxb issue

--- project.clj
+++ project.clj
@@ -11,6 +11,12 @@
 		 [org.apache.activemq/activemq-all "5.15.2"]
 		 [org.apache.commons/commons-collections4 "4.1"]
  		 [org.slf4j/slf4j-simple "1.7.36"]
-		 [com.github.redhatqe/polarize-metadata "0.1.1-SNAPSHOT"]]
+		 [com.github.redhatqe/polarize-metadata "0.1.1-SNAPSHOT"]
+		 [com.sun.xml.bind/jaxb-impl "3.0.2"]
+		 [com.sun.xml.bind/jaxb-core "3.0.2"]
+ 		 [javax.xml.bind/jaxb-api "2.3.1"]
+		 ;;[jakarta.xml.bind/jakarta.xml.bind-api "3.0.1"]
+		 ]
+		 
   :javac-options {:debug "on"}
   :plugins [[lein2-eclipse "2.0.0"]])
